a385ac8c162586dbcd34ef3504d463cdff72f0f6
Emily Wang <emmwang@google.com>
2023-04-13 11:36:23 -0400

    test: separate gcp_ubuntu_docker setup from macos_external setup
    * Using DOCKER_IP directly requires configuration of insecure registries; try resolve-to-localhost workaround

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -11,9 +11,9 @@ source github/jib/kokoro/docker_setup.sh
 fi
 
 if [ "${KOKORO_JOB_CLUSTER}" = "GCP_UBUNTU_DOCKER" ]; then
-export DOCKER_IP="$(/sbin/ip route|awk '/default/ { print $3 }')"
-echo "DOCKER_IP: ${DOCKER_IP}"
-echo "${DOCKER_IP} localhost" >> /etc/hosts
+export DOCKER_HOST_IP="$(/sbin/ip route|awk '/default/ { print $3 }')"
+echo "DOCKER_HOST_IP: ${DOCKER_HOST_IP}"
+echo "${DOCKER_HOST_IP} localhost" >> /etc/hosts
 mkdir /tmpfs/auth
 docker run --entrypoint htpasswd httpd:2 -Bbn testuser testpassword > /tmpfs/auth/htpasswd
 fi
